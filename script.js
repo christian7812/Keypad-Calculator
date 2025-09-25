b681d93991f2a89239c7fbf2cbaadf7fd9b4feca
Christian Cruiser Atsagba <christiancruiseratsagba@gmail.com>
2025-09-25 02:01:25 +0000

    Update files

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ button.forEach(btn=> {
         }
         //Equal Button
         else if(btn.classList.contains("equal")) {
-            if(!firstNumber !== null && operator && currentNumber !== "") {
+            if(firstNumber !== null && operator && currentNumber !== "") {
                 const output = operate(firstNumber, parseFloat(currentNumber), operator);
                 display.value = output;
                 firstNumber = output;
@@ -67,10 +67,73 @@ button.forEach(btn=> {
     })
 })
 
-   //Reset Calculator
-       function resetCalculator() {
-        currentNumber= "";
-        operator = null;
-        firstNumber = null;
-        shouldReset = false;
-       } 
+
+//Mobile Phone
+const displayInput = document.getElementById("displayInput");
+const buttons = document.querySelectorAll(".btn");
+
+let currentNum = "";
+let firstNum = null;
+let operators = null;
+let shouldResets = false;
+
+buttons.forEach(button=> {
+    button.addEventListener("click", () => {
+        const outcome = button.textContent;
+
+        //Delete Button
+        if(button.classList.contains("Delete")) {
+            currentNum = currentNum.slice(0,-1);
+            displayInput.value = currentNum;
+        }
+        //Clear Button
+        else if(button.classList.contains("clearAll")) {
+            currentNum ="";
+            operators = null;
+            firstNum = null;
+            displayInput.value = "";
+        }
+        //Equal Button
+        else if(button.classList.contains("equalTo")) {
+            if(firstNum !== null && operators && currentNum !== "") {
+                const output = operation(firstNum, parseFloat(currentNum), operators);
+                displayInput.value = output;
+                firstNum = output;
+                operators = null;
+                shouldResets = true;
+            }
+        }
+        //Operator Button
+        else if(["+","-","*","/"].includes(outcome)) {
+            if(currentNum !== "") {
+             firstNum = parseFloat(currentNum);
+            } else if(operators) {
+            firstNum = operation(firstNum,parseFloat(currentNum),operators);
+            displayInput.value = firstNum;
+            }
+            operators = outcome;
+            currentNum = "";
+        }
+        //ShouldReset If It's Not the above Options
+        else{
+            if(shouldResets) {
+                currentNum ="";
+                displayInput.value = "";
+                shouldResets = false;
+            }
+            currentNum += outcome;
+            displayInput.value = currentNum;
+        }
+
+        function operation(a,b,op) {
+            switch(op) {
+                case"+": return a + b;
+                case"-": return a- b;
+                case"*": return a * b;
+                case"/": return b !== 0 ? a / b : "Syntax Error";
+                default: return b;
+            }
+        }
+          
+    })
+})
